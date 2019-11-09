73bf8daf7fcb94798cc9c76e2fb4e5babd5365a1
Ryan-Donovan33 <55716909+Ryan-Donovan33@users.noreply.github.com>
2019-11-08 21:20:33 -0500

    Finished Classes, Now High Order Functions

--- challenges/classes.js
+++ challenges/classes.js
@@ -12,18 +12,31 @@ class CuboidMaker {
 		this.width = width;
 		this.height = height;
 	}
+
+	volume() {
+		return `${this.length}` * `${this.width}` * `${this.height}`;
+	}
+
+	surfaceArea() {
+		return (
+			2 *
+			(`${this.length}` * `${this.width}` +
+				`${this.length}` * `${this.height}` +
+				`${this.width}` * `${this.height}`)
+		);
+	}
 }
 
-CuboidMaker.prototype.volume = function() {
-	return `${this.length}` * `${this.width}` * `${this.height}`;
-};
+// CuboidMaker.prototype.volume = function() {
+// 	return `${this.length}` * `${this.width}` * `${this.height}`;
+// };
 
-CuboidMaker.prototype.surfaceArea = function() {
-	return (
-		2 *
-		(`${this.length}` * `${this.width}` + `${this.length}` * `${this.height}` + `${this.width}` * `${this.height}`)
-	);
-};
+// CuboidMaker.prototype.surfaceArea = function() {
+// 	return (
+// 		2 *
+// 		(`${this.length}` * `${this.width}` + `${this.length}` * `${this.height}` + `${this.width}` * `${this.height}`)
+// 	);
+// };
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 // console.log(cuboid.volume()); // 100
